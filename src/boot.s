09b55daf3b75310e57c638feb0871b97212ce88b
Radoslaw Kujawa <strim@c0ff33.net>
2017-07-20 22:44:26 +0200

    Make this actually build a proper ROM image.
    This is now compatible with Hatari, without the need to modify the
    emulator.

--- src/boot.s
+++ src/boot.s
@@ -1,9 +1,19 @@
 	code
 
 entry_rom:
-	nop
-.1:	jmp		.1
+	dc.w		$0000
+	dc.w		$0000
 
+	dc.l		$fc0008
+
+	nop
+.1	nop
+	jmp		.1
+
+
+	section		"romend",code
+
+	dc.w		$55AA
 
 ; vim: set ft=asm68k:
 
