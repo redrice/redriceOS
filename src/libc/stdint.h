1fbb804dd0e6dbd1d0fe93569499d14d8a2cc0af
Radoslaw Kujawa <strim@c0ff33.net>
2017-07-25 22:36:59 +0200

    Add stdint.h - first file of our own micro C library.

---
+++ src/libc/stdint.h
@@ -0,0 +1,36 @@
+#ifndef _STDINT_H_
+#define _STDINT_H_
+
+#ifndef int8_t
+typedef signed char int8_t;
+#endif
+
+#ifndef uint8_t
+typedef unsigned char uint8_t;
+#endif
+
+#ifndef int16_t
+typedef short int int16_t;
+#endif
+
+#ifndef uint16_t
+typedef unsigned short int uint16_t;
+#endif
+
+#ifndef int32_t
+typedef int int32_t;
+#endif
+
+#ifndef uint32_t
+typedef unsigned int uint32_t;
+#endif
+
+#ifndef intptr_t
+typedef int intptr_t;
+#endif
+
+#ifndef uintptr_t
+typedef unsigned int uintptr_t;
+#endif
+
+#endif /* _STDINT_H_ */
