8ccf991c2ec351346b9f1aac2d6fbc80bbf741c7
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2017-08-17 10:40:39 +0200

    Better comments.

--- src/boot.s
+++ src/boot.s
@@ -9,16 +9,19 @@ CARTMAGIC equ $fa52235f
 	code
 
 entry_rom:
-
+	; ID used by TOS to jump early into code on cartridge.
 	dc.l		CARTMAGIC
 
-	move		#$2700,sr	; supervisor mode
+	; Theoretically CPU should be in supervisor mode now, but 
+	; someone could have performed "warm" reboot by jumping into
+	; this location.
+	move		#$2700,sr	; supervisor mode, interrupts disabled
 
-	reset
+	reset				; reset peripherals
 
-	jmp		memory_init
+	jmp		memory_init	; init the memory controller
+
 memory_configured:
-
 	movea.l		_phystop,sp	; set stack pointer to the end of RAM
 	move.l		_phystop,d6	; save phystop in d6, survive bss_clear
 
@@ -30,6 +33,8 @@ memory_configured:
 
 	jsr		_main		; jump into C kernel code
 
+	; This section is used to mark end of ROM. Linker automatically fills
+	; empty space in ROM with zeros.
 	section		"romend",code
 
 	dc.w		$55AA
